42ac5c828159d6f5fb20006580752c0da27a57a0
roger yee <ryee.epicodus@gmail.com>
2018-03-16 08:53:14 -0700

    builds initial files and folders

---
+++ spec/age-calculator-spec.js
@@ -0,0 +1,8 @@
+// import { Constructor } from './../src/age-calculator.js'
+
+describe('', function() {
+  it('', function() {
+    // var example = new Constructor(parameters)
+    // expect(example.method()).toEqual('');
+  });
+});
---
+++ src/main.js
@@ -0,0 +1,8 @@
+import './styles.css';
+import $ from 'jquery';
+import 'bootstrap/dist/css/bootstrap.min.css';
+//import { **insert class name** } from './age-calculator.js';
+
+$(document).ready(function() {
+
+});
